b5bf209ea1a18323acb86eb04f76428113f62dd6
Dylan Jules <dylan.jules@oakwood.edu>
2025-06-17 01:55:53 -0400

    This is the fitness tracker

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./layout/Layout";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "./auth/AuthContext";
 import { ApiProvider } from "./api/ApiContext";
 import { PageProvider } from "./layout/PageContext";
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <ApiProvider>
-      <PageProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </PageProvider>
-    </ApiProvider>
-  </AuthProvider>,
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ApiProvider>
+          <PageProvider>
+            <Layout>
+              <App />
+            </Layout>
+          </PageProvider>
+        </ApiProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
